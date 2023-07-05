7354e80e7a7bed6eeb6efe04cfb56a9152d1ee3e
Daisel10 <davidsanchezlombana10@gmail.com>
2023-07-04 20:45:26 -0500

    👌👌👌

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
+
+// Configuración de multer para manejar la carga de archivos
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -9,6 +13,20 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Ruta de carga de archivos
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (req.file) {
+    const { originalname, mimetype, size } = req.file;
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size: size
+    });
+  } else {
+    res.status(400).json({ error: 'No file uploaded' });
+  }
 });
 
 
